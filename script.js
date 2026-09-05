57c8e6a64c2973b59f228cb65e1457b91b7109b7
kingkarma-cpu <osegbov1@gmail.com>
2026-09-05 15:58:57 +0000

    co

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ const action = [
       url: "https://www.youtube.com/embed/vtQp7Xpz1l0"
     },
     {number: 2,
-      url: "https://animepahe.pw/play/50687a43-d10f-a6f2-4209-02264788df7c/063b24f07dab6875b64ba04ecebf8d027f21be6350845de9e1b6a1b75a47e887"
+      url: "https://animekai.be/embed/code-geass-lelouch-of-the-rebellion-r2/ep-1"
     }
   ]
 },
